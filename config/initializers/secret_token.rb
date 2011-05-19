2e8f4c72c09f546cd51de0a085ec1cac1618837c
James Kennedy <james@jameskennedy.ie>
2011-05-19 17:25:15 -0400

    initial install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluefacelog::Application.config.secret_token = '50c4fc12c27b7e97c6b2b46b7652b3eb5522337054827e271e91a5ac277cc9cb327a9b88951d61a41afd095a9be995ed64810ec30a1ed42a62260cf5b68616ed'
